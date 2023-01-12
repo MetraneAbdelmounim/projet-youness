7d461c616a9a3d4d84063bffc9009d2fd6ef90c6
MetraneAbdelmounim <abdelmounim.metrane@gmail.com>
2023-01-11 19:57:39 -0500

    Testing

--- backend/python/app.py
+++ backend/python/app.py
@@ -6,27 +6,29 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
-from pymodbus.client import ModbusTcpClient as ModbusClient
+
+#from pymodbus.client import ModbusTcpClient as ModbusClient
+from pymodbus.client import AsyncModbusTcpClient as ModbusClient
 from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadDecoder
 from pymodbus.payload import BinaryPayloadBuilder
 
 
 @app.route('/mppt/<ip>',methods=['GET'])
-def getDataMppt(ip):
+async def getDataMppt(ip):
     Mppt = mppt(0,0,0,0,0,0,0,0)
-    try:
-        print("hello")
-        c = ModbusClient(host=ip, port=502,strict=False,timeout=20)
-        c.connect()
-        time.sleep(20)
-        rr = c.read_holding_registers(0,82,2)
+    try :
+
+        c =ModbusClient(host=ip, port=502,unit_id=1, auto_open=True,timeout=10)
+        await c.connect()
+        time.sleep(2)
+        rr = await c.read_holding_registers(0,82,2)
         print(rr)
         Mppt = mppt(rr.registers[24],rr.registers[28],rr.registers[27],rr.registers[16],rr.registers[19],rr.registers[62],rr.registers[20],rr.registers[22])
         #Mppt=mppt(20065,20065,20065,20065,20065,20065,20065,20065)
 
-    except:
-        
+    except Exception as e:
+        print(e)
         Mppt = mppt(0,0,0,0,0,0,0,0)
 
     return json.dumps(Mppt.__dict__)
