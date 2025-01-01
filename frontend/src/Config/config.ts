9c26b5b1b759c411492e999c05520b7376ac0c85
MetraneAbdelmounim <abdelmounim.metrane@gmail.com>
2025-01-02 00:32:34 +0100

    Application v2.0

--- frontend/src/Config/config.ts
+++ frontend/src/Config/config.ts
@@ -1,5 +1,7 @@
 export const config = {
   durationMessage: 5000,
   itemsPerPage : 20,
-  Battery_Max : 25
+  Battery_Max_LTH : 24.5,
+  Battery_Max_AGM : 23
+
 };
--- frontend/src/app/dashbord/dashbord.component.ts
+++ frontend/src/app/dashbord/dashbord.component.ts
@@ -8,6 +8,7 @@ import{config} from "../../Config/config"
 
 import {Site} from "../models/site";
 import {NzMessageService} from "ng-zorro-antd/message";
+import { range } from 'rxjs';
 @Component({
   selector: 'app-dashbord',
   templateUrl: './dashbord.component.html',
@@ -25,6 +26,7 @@ export class DashbordComponent implements OnInit,OnDestroy {
   labels_bar: Array<string>=new Array<string>()
   colors : Array<string>=new Array<string>()
   maxVolt : Array<Number>=new Array<Number>()
+  maxAGM : Array<Number>=new Array<Number>()
   spinnerSite: boolean=false;
   upVoltage:Number=0
   // @ts-ignore
@@ -52,12 +54,13 @@ export class DashbordComponent implements OnInit,OnDestroy {
           this.labels.push(s.nom)
           this.battery.push(s.Battery_Voltage)
           this.labels_bar.push(s.ip)
-          this.maxVolt.push(config.Battery_Max)
-          this.colors.push(this.getColor(s.Battery_Voltage>config.Battery_Max?true:false))
+          this.maxVolt.push(config.Battery_Max_LTH)
+          this.maxAGM.push(config.Battery_Max_AGM)
+          this.colors.push(this.getColor(s.Battery_Voltage>config.Battery_Max_LTH?true:false))
         })
         
   
-        this.createChart(this.labels,this.battery,this.colors,this.labels_bar,this.maxVolt)
+        this.createChart(this.labels,this.battery,this.colors,this.labels_bar,this.maxVolt,this.maxAGM)
         
         this.upVoltage=(upSite/sites.length)*100
         this.sites=sites
@@ -81,42 +84,74 @@ export class DashbordComponent implements OnInit,OnDestroy {
     this.chart.destroy()
   }
 
-  createChart(labels: Array<string>, battery: Array<any>, colors: Array<string>, labels_bar: Array<any>,maxVolt:Array<any>): void {
+  createChart(labels: Array<string>, battery: Array<any>, colors: Array<string>, labels_bar: Array<any>,maxVolt:Array<any>,maxAGM:Array<any>): void {
     Chart.register(...registerables);
     const data = {
       labels: labels,
       datasets: [{
         backgroundColor: colors,
-        borderColor:"#005e03",
+        borderColor:"#003366",
         borderWidth:1,
         data: battery,
         label:"Battery Voltage",
         order:2
       },{
-        label:"Voltage Lithium Battery",
+        label:"Max Voltage Lithium Battery",
+        backgroundColor: "#000136",
         type: 'line' as ChartType,
-        borderColor:"#a20000",
+        borderColor:"#000136",
         borderWidth:2,
         data: maxVolt,
         order:1
-      }]
+      },
+        {
+          label:"Max Voltage AGM Battery",
+          backgroundColor: "#e0baba",
+          type: 'line' as ChartType,
+          borderColor:"#e0baba",
+          borderWidth:2,
+          data: maxAGM,
+          order:1
+
+        },
+        {
+          label:"Low Battery",
+          backgroundColor:"#e65757",
+          data: [],
+          order : 3,
+
+        },
+        {
+          label:"Good Battery",
+          backgroundColor:"#63c985",
+          data: [],
+          order : 4,
+        },
+
+      ]
     };
     const options = {
       scales: {
         y: {
-          beginAtZero: true,
+          min: 22,
+          max:27,
           display: true,
-          stacked:true
+          stacked:false,
+          ticks: {
+            // forces step size to be 50 units
+            stepSize: 0.2
+          }
         },
         x:{
           stacked: true,
         }
       },
+
       plugins: {
         title: {
           display: true,
           text: 'Battery Voltage Monitoring',
-          color: '#911',
+          color: '#003366',
           font: {
             family: 'Comic Sans MS',
             size: 20,
@@ -124,19 +159,54 @@ export class DashbordComponent implements OnInit,OnDestroy {
             lineHeight: 1.2,
           },
         },
+        legend: {
+          labels: {
+                // @ts-ignore
+            generateLabels: (chart) => {
+              const original = Chart.defaults.plugins.legend.labels.generateLabels!;
+              const labels = original(chart);
+
+              // Filter out the label you want to hide (e.g., "Label 2").
+              return labels.filter(label => label.text !== "Battery Voltage");
+            },
+          },
+        },
 
       },
 
     }
+    const image = new Image();
+    image.src = 'assets/images/logo.png'; // Replace with the path to your logo
+    image.onload = () => {
 
+      const backgroundLogoPlugin = {
+        id: 'backgroundLogo',
+        beforeDraw: (chart: any) => {
+          const { width, height, left, top } = chart.chartArea;
+          const ctx = chart.ctx;
 
-    const config: ChartConfiguration = {
-      type: 'bar',
-      data: data,
-      options: options
+          // Draw the logo at the center of the chart
+          const x = left + (width - image.width) / 2;
+          const y = top + (height - image.height) / 2;
+
+          ctx.save();
+          ctx.globalAlpha = 0.25; // Adjust transparency
+          ctx.drawImage(image, x, y, image.width, image.height);
+          ctx.restore();
+        },
+      };
+
+      // Register the plugin
+      Chart.register(backgroundLogoPlugin);
+      const config: ChartConfiguration = {
+        type: 'bar',
+        data: data,
+        options: options
+      }
+      const chartItem: ChartItem = document.getElementById('my-chart') as ChartItem
+      this.chart = new Chart(chartItem, config)
     }
-    const chartItem: ChartItem = document.getElementById('my-chart') as ChartItem
-    this.chart = new Chart(chartItem, config)
+    
   }
 
   getRandomColor():string {
@@ -161,7 +231,7 @@ export class DashbordComponent implements OnInit,OnDestroy {
   }
 
   private getColor(isValid: boolean) {
-    if(isValid) return "#61e18a"
-    else return "#ff5454"
+    if(isValid) return "rgba(99, 201, 133, 0.80)"
+    else return "rgba(230, 87, 87, 0.8)"
   }
 }
