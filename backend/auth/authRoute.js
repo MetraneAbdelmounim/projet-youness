874e0d7e3cc73dad4f7204d0deeb01db8b2c6f69
MetraneAbdelmounim <abdelmounim.metrane@gmail.com>
2025-01-02 16:40:38 +0100

    Application v2.0 Add Licence

--- backend/auth/authRoute.js
+++ backend/auth/authRoute.js
@@ -5,6 +5,6 @@ const authUser = require('../middlewares/authUser')
 
 const licenceGuard= require('../middlewares/licenceGuard')
 router.post('/login',licenceGuard,authController.login);
-router.get("/members/tokens/:token",licenceGuard,authUser,authController.getMemberFromToken)
+router.get("/members/tokens/:token",authUser,authController.getMemberFromToken)
 router.put("/members/logout/:idMember",authUser,authController.logoutMember)
 module.exports = router;
