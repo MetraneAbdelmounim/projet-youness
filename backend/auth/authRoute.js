75f638dbf0cff1070e7d99996e091af76657cb34
MetraneAbdelmounim <abdelmounim.metrane@gmail.com>
2025-05-09 00:04:40 +0100

    Application front refont V2.0

--- backend/auth/authRoute.js
+++ backend/auth/authRoute.js
@@ -4,7 +4,7 @@ let router = express.Router();
 const authUser = require('../middlewares/authUser')
 
 const licenceGuard= require('../middlewares/licenceGuard')
-router.post('/login',authController.login);
+router.post('/signin',authController.login);
 router.get("/members/tokens/:token",authUser,authController.getMemberFromToken)
 router.put("/members/logout/:idMember",authUser,authController.logoutMember)
 module.exports = router;
