68cdb3b8cb05a30c897180eb6ac70cbcc4dcf2fd
MetraneAbdelmounim <abdelmounim.metrane@gmail.com>
2025-05-09 00:48:01 +0100

    Application front refont V2.0

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
 
-  HOST: process.env.HOST || '0.0.0.0',
+  HOST: process.env.HOST || '127.0.0.1',
   PORT: process.env.PORT || 5000,
   bdUrl : (process.env.NODE_ENV=="production")?"mongodb://mongo:27017/mppt":"mongodb://127.0.0.1:27017/mppt",
   secret_token_key:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ",
