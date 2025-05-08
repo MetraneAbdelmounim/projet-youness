48200cd9506094ea2217a5ca202d31be5c5b473a
MetraneAbdelmounim <abdelmounim.metrane@gmail.com>
2025-05-08 16:55:03 +0100

    Application front refont V2.0

--- backend/app.js
+++ backend/app.js
@@ -37,10 +37,10 @@ app.use(logger('dev'));
 
 
 app.use('/uploads',express.static('uploads'));
-app.use('/',express.static(path.join(__dirname,'public/browser')));
+app.use('/',express.static(path.join(__dirname,'public')));
 app.use('/uploads',express.static(path.join(__dirname,'uploads')));
 app.get('', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/browser','index.html'));
+    res.sendFile(path.join(__dirname, 'public','index.html'));
 });
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
