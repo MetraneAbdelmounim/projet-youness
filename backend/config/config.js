64bb09d127d5d575841de8c76d6ce37f8813b9f6
MetraneAbdelmounim <abdelmounim.metrane@gmail.com>
2025-06-14 13:19:42 +0100

    Fix bug date

--- backend/config/config.js
+++ backend/config/config.js
@@ -22,6 +22,6 @@ module.exports = {
         pass: 'Axians@2024'
     }  
   },
-  schedule:(process.env.NODE_ENV=="production")? '*/15 * * * *' : '*/60 * * * *'
+  schedule:(process.env.NODE_ENV=="production")? '*/2 * * * *' : '*/60 * * * *'
 
 }
